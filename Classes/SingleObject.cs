d4f7c1ab9d0d2179ff5286f841f0231571e8c137
mtachot <matthieu.tachot@hotmail.fr>
2017-07-19 09:52:25 +0200

    First commit

---
+++ Classes/SingleObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest.Classes
+{
+    public class SingleObject
+    {
+        private static SingleObject instance = new SingleObject();
+
+        // Le constructeur est rendu privé, ce qui empêche l'instanciation en dehors de la classe
+        private SingleObject(){}
+
+        public static SingleObject getInstance()
+        {
+            return instance;
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello world !");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using SingletonTest.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemple d'instanciation interdite avec un singleton :
+            // SingleObject obj = new SingleObject();
+
+            SingleObject obj = SingleObject.getInstance();
+            obj.ShowMessage();
+            Console.ReadLine();
+        }
+    }
+}
